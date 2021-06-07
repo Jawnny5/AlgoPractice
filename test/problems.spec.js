97ea2de5d1a6e4f0c42dd4f6a523a8a63ac5abb4
“Jawnny5” <william.neal83@gmail.com>
2021-06-07 11:46:37 -0600

    removing urlify test and checking if commits are tracking

--- test/problems.spec.js
+++ test/problems.spec.js
@@ -18,7 +18,3 @@ it('checks if a string is a permutation of another', () => {
 it('checks if a string is a palindrome', () => {
   assert.equal(uniqueString('abba'), true)
 })
-
-it('replaces spaces in a given string with %20', () => {
-  assert.equal(urlify('bomb dot com'), 'bomb%20dot%20com')
-})
